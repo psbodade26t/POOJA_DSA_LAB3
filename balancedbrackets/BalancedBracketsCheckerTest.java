97373f44c3171bd9b921b7bd8dfd60f0a3830a2d
psbodade26t <111677483+psbodade26t@users.noreply.github.com>
2022-11-08 23:45:40 +0530

    Add files via upload

---
+++ balancedbrackets/BalancedBracketsCheckerTest.java
@@ -0,0 +1,32 @@
+package com.LAB_3DSA.balancedbrackets;
+
+public class BalancedBracketsCheckerTest {
+	public static void main(String[] args) {
+
+		test("");
+		test("([[{}]])");
+
+		test("([[{}]]))");
+
+		test("[]");
+
+		test("[)");
+
+		test("[[[]]]");
+
+		test("M{{[[[()]]]}}W");
+		test("M{{[[[()]]]}}");
+	}
+
+	static void test(String brackets) {
+
+		BalancedBracketsChecker checker = new BalancedBracketsChecker(brackets);
+
+		boolean result = checker.check();
+		if (result) {
+			System.out.println("Bracket expression " + brackets + " is balanced");
+		} else {
+			System.out.println("Bracket expression " + brackets + " is NOT balanced");
+		}
+	}
+}
